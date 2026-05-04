6bb075678976a4484ad44802e98f85b28b659ed7
LiquidPlummer <kyle.plummer@revature.com>
2026-05-04 14:04:20 -0400

    demo 1

--- scala-1/hello.scala
+++ scala-1/hello.scala
@@ -2,7 +2,9 @@ object Hello {
     def main(args: Array[String]): Unit = {
         println("Hello, Scala!")
         demo1()
-
+        DemoObject.stringInterpOne()
+        DemoObject.stringInterpTwo()
+        DemoObject.stringInterpThree()
     }
 
     
@@ -32,6 +34,55 @@ object Thing {
     }
 }
 
+
+
+
+// Statement vs expression?
+//If is a statement, not an expression in most languages
+// What is the special thing like "if" that allows predicate assignment? Ternary operator
+// In Scala there is no ternary operator... we don't need it... why? In Scala "If"s are expressions!
+
+
+
+// Expressions resolve to a value
+// Invoking functions/methods are expressions
+// math: 2+2, 2-3, + - / * %
+// And also: if statements can be expressions
+
+
+// val status = if (score >= 60) "pass" else "fail" //This is effectively a ternary expression
+
+// val result = {
+//   val a = 10
+//   val b = 20
+//   a + b      // This is the value of the block
+// }
+
+
+object DemoObject {
+    def stringInterpOne(): Unit = {
+        val name = "Alice"
+        val fullName = "Alice" + " " + "Smith"
+        val greeting = s"Hello, $name, your full name is: $fullName"
+        println(greeting)
+    }
+
+    def stringInterpTwo(): Unit = {
+        val price: Double = 3.14159
+        println(f"Price: $$$price%.2f")
+    }
+
+    def stringInterpThree(): Unit = {
+        val testVal = "test"
+        val multiLineString = s"""This is a multi-line 
+string. The whitespace and newline
+characters are maintained.
+$testVal
+"""
+        println(multiLineString)
+    }
+
+}
 
 
 
--- scala-1/run.sh
+++ scala-1/run.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 # This will compile all scala files in the directory, then execute based on which object was given
-# ./run.sh Hello
+
 if [ -z "$1" ]; then
   echo "Usage: ./run.sh <ObjectName>"
   exit 1
 fi
 
-scalac *.scala && scala "$1"
+mkdir -p target
+scalac -d target *.scala && scala -cp target "$1"
